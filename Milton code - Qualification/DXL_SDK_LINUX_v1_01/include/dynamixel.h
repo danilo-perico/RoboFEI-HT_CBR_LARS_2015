26bdfd959ee91d9ed92f2013ac97eb677b2e497b
danilo-perico <danilo.perico@gmail.com>
2014-04-06 18:06:36 -0300

    Milton robot - code used for Robocup 2014 qualification
    main header file

--- Milton code - Qualification/DXL_SDK_LINUX_v1_01/include/dynamixel.h
+++ Milton code - Qualification/DXL_SDK_LINUX_v1_01/include/dynamixel.h
@@ -12,7 +12,9 @@ extern "C" {
 #define P_PRESENT_POSITION_L	36
 #define P_PRESENT_POSITION_H	37
 #define P_MOVING		46
-#define MOVING_SPEED            32
+#define MOVING_SPEED    32
+#define DEFAULT_BAUDNUM    1
+
 
 
 ///////////// device control methods ////////////////////////
@@ -81,9 +83,50 @@ void dxl_write_byte(int id, int address, int value);
 int dxl_read_word(int id, int address);
 void dxl_write_word(int id, int address, int value);
 
-void roboereto();
-void chuteretodireita();
+#define KEEP_WALKING *(mem+2)
+
+#define LEG_RIGHT 0
+#define LEG_LEFT 1
+
+//////////// extra functions ///////////////////////
+void levantar_fitPC();
 void espera_mov();
+void robo_ereto();
+void robo_ereto_corrigido();
+void andar(int passos);
+void andar_rapido_ik();
+void virar(int passos);
+void chutepedireito_fitPC();
+void chutepedireito();
+void inclinar();
+void levanta_abaixa_perna_dir();
+void levanta_abaixa_perna_esq();
+void levantar_de_barriga();
+void levantar_de_costas_fitPC();
+void levantar_de_costas();
+int visao();
+float distancia_bola();
+float angle_bola();
+void andar_lateral_direita(int passos);
+void andar_lateral_esquerda(int passos);
+void andar_curto(int passos);
+void andar_rapido(int passos);
+void espera_mov_ID(int id);
+
+void inverse_kinematic_right_leg(float x, float y_e, int inclin, int VEL);
+void inverse_kinematic_left_leg(float x, float y_e, int inclin, int VEL);
+void espera_mov_ID(int id);
+void espera_mov();
+void inclina_plano(int select_leg, int angle, int vel, int offset_pe);
+
+
+////global variables/////////////////////////
+extern int inclina;
+extern unsigned int StandupPos[22];
+extern unsigned int StandupPos_corrigido[22];
+extern unsigned int StandupPos_corrigido2[22];
+extern int *mem;
+
 
 
 #ifdef __cplusplus
