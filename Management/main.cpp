b5c74b0ab0dee816f09569ff60031461e21c5a90
Thiago <thiago@bambu-pc.(none)>
2014-04-07 15:13:53 -0300

    v.0.0.1

---
+++ Management/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+int spawn (char* program, char** arg_list)
+{
+  pid_t child_pid;
+  /* Duplicate this process. */
+  child_pid = fork ();
+  if (child_pid != 0)
+       /* This is the parent process. */
+      return child_pid;
+  else {
+       /* Now execute PROGRAM, searching for it in the path. */
+       execvp (program, arg_list);
+       /* The execvp function returns only if an error occurs. */
+       fprintf (stderr, "an error occurred in execvp\n");
+       abort ();
+   }
+}
+
+int main()
+{
+    char* arg_list[] = {
+    NULL
+    };
+    spawn ("/home/thiago/Milton shared Memory 26-02-2014/Decisao/bin/Debug/Decisao", arg_list);
+    spawn ("/home/thiago/planning/bin/Debug/planning",arg_list);
+    return 0;
+}
---
+++ Planning/planning.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <assert.h>
+
+#include "planning.h"
+
+
+void planning::stop()
+{
+}
+
+void planning::turn_left()
+{
+}
+
+void planning::turn_right()
+{
+}
+
+void planning::kick_left_strong()
+{
+}
+
+void planning::kick_left_weak()
+{
+}
+
+void planning::kick_right_strong()
+{
+}
+
+void planning::kick_right_weak()
+{
+}
+
+void planning::fall_right()
+{
+}
+
+void planning::fall_left()
+{
+}
+
+void planning::fall_forward()
+{
+}
+
+void planning::fall_backward()
+{
+}
+
+void planning::up_forward()
+{
+}
+
+void planning::up_backward()
+{
+}
