2173277f5d34da6f0d348f189fd0c1fbaed8ae4a
root <root@bambu-pc.(none)>
2014-04-08 15:52:58 -0300

    v.0.0.1

--- Planning/main.cpp
+++ Planning/main.cpp
@@ -1,79 +1,46 @@
+/*--------------------------------------------------------------------
+
+******************************************************************************
+  * @file       Planning/main.cpp
+  * @author     Thiago Pedro Donadon Homem - ROBOFEI-HT - FEI
+  * @version    V0.0.0
+  * @created    07/04/2014
+  * @Modified   07/04/2014
+  * @e-mail     thiago.homem@gmail.com
+  * @brief      Planning
+  ****************************************************************************
+
+  Arquivo fonte contendo as funções de planejamento.
+
+/--------------------------------------------------------------------*/
+
+
 #include <iostream>
-#include "planning.h"
-#include <errno.h>
 #include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include "planning.h"
+#include "../blackboard.h"
 
-#define KEY 123
-
-//Backboard definitions
-#define TYPE_FUNCTION *(mem+1)// 0 - Nenhuma função
-                               // 1 - Função Andar rápido
-                               // 2 - Função Andar curto
-                               // 3 - Função Virar
-                               // 4 - Função Andar lateral direita
-                               // 5 - Função Andar lateral esquerda
-                               // 6 - Chute pé direito
-#define planning_command *(mem+2)
-#define planning_parameter_vel *(mem+3)
-#define planning_parameter_angle *(mem+4)
-#define planning_parameter_r *(mem+5)
-
-#define decision_action_a *(mem+6)
-#define decision_action_b *(mem+7)
 
 using namespace std;
 
-int *mem ; //Variável que manipula memória compartilhada
+
 
 int main()
 {
-    // --- Variaveis usada para memoria compartilhada -----
-    int shmid ;  // identificador da memoria comum //
-    int size = 1024 ;
-    char *path="nome_de_arquivo_existente" ;
-    int flag = 0;
-
-    //
-    // Recuperando ou criando uma memoria compartilhada-------------------
-    //
-     if (( shmid = shmget(ftok(path,(key_t)KEY), size,0)) == -1)
-     {
-          perror("Erro no shmget") ;
-          printf("\nMemória será criada\n");
-         //return(1) ;
-        if (( shmid = shmget(ftok(path,(key_t)KEY), size, IPC_CREAT|IPC_EXCL|SHM_R|SHM_W)) == -1)
-        {
-            perror("Erro no shmget") ;
-            return(1) ;
-        }
-
-     }
-
-     printf("Sou o processo com pid: %d \n",getpid()) ;
-     printf("Identificador do segmento recuperado: %d \n",shmid) ;
-     printf("Este segmento e associado a chave unica: %d\n",ftok(path,(key_t)KEY)) ;
-    //
-    // acoplamento do processo a zona de memoria
-    //recuperacao do pornteiro sobre a area de memoria comum
-    //
-     if ((mem = (int*)shmat (shmid, 0, flag)) == (int*)-1){
-          perror("acoplamento impossivel") ;
-          return (1) ;
-     }
-    *mem = 0;
+     using_shared_memory(); //Função que cria e acopla a memória compartilhada
 
     while(1){
        //teste de leitura de memória
-       cout << planning_command << endl;
-       cout << planning_parameter_vel << endl;
-       cout << planning_parameter_angle << endl;
-       cout << planning_parameter_r << endl;
-
-       cin >> decision_action_a;
-       cin >> decision_action_b;
-
+       cout << PLANNING_COMMAND << endl;
+       cout << PLANNING_PARAMETER_VEL << endl;
+       cout << PLANNING_PARAMETER_ANGLE << endl;
+       DECISION_ACTION_A = 1;
+       cout << DECISION_ACTION_A<<endl;
+       cout << DECISION_ACTION_B<<endl;
+       sleep(1);
+       //cin >> DECISION_ACTION_A;
+       //cin >> DECISION_ACTION_B;
+       //sleep(1);
     }
 
     return 0;
