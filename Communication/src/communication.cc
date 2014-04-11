92c2c3e446d50325cbd5d9f3d195ca16b89ec908
danilo-perico <danilo.perico@gmail.com>
2014-04-11 10:45:54 -0300

    v0.0.2
    dummy version

--- Communication/src/communication.cc
+++ Communication/src/communication.cc
@@ -1,6 +1,22 @@
+/*--------------------------------------------------------------------
+
+******************************************************************************
+  * @file       communication.cc
+  * @author     Danilo Hernnai Perico - ROBOFEI-HT - FEI
+  * @version    V0.0.1
+  * @created    08/04/2014
+  * @Modified   10/04/2014
+  * @e-mail     danilo.perico@gmail.com
+  * @brief      Communication
+  ****************************************************************************
+/--------------------------------------------------------------------*/
+
+
 #include <iostream>
 #include <communication.h>
-#include "blackboard.h"
+#include <blackboard.h>
+#include<pthread.h>
+#include <unistd.h>
 
 #define DEBUG
 //#define _cpluplus
@@ -17,20 +33,64 @@ communication::~communication()
 }
 #endif
 
+
 int main(void)
 {
-
+int A, B, C, D, E, F, G, H, I, J;
 using namespace std;
+
+using_shared_memory();
 
 #ifdef DEBUG
 cout << "Communication" << endl;
 #endif
 
-using_shared_memory();
+while(1)
+{
+for (int i = 0;i<100000;i++)
+{
+//SENT VARIABLES TO DECISION
+COM_ACTION_ROBOT1 = 1;
+COM_ACTION_ROBOT2 = 2;
+COM_ACTION_ROBOT3 = 3;
+COM_STATE_ROBOT1 = 10;
+COM_STATE_ROBOT2 = 11;
+COM_STATE_ROBOT3 = 12;
+COM_POS_ROBOT1 = 20 ;
+COM_POS_ROBOT2 = 21;
+COM_POS_ROBOT3 = 22;
+COM_POS_BALL_ROBOT1 = 30;
+COM_POS_BALL_ROBOT2 = 31;
+COM_POS_BALL_ROBOT3 = 32;
+COM_POS_OPP_A_ROBOT1 = 40;
+COM_POS_OPP_A_ROBOT2 = 41;
+COM_POS_OPP_A_ROBOT3 = 42;
+COM_POS_OPP_A_ROBOT4 = 43;
+COM_POS_OPP_B_ROBOT1 = 50;
+COM_POS_OPP_B_ROBOT2 = 51;
+COM_POS_OPP_B_ROBOT3 = 52;
+COM_POS_OPP_B_ROBOT4 = 53;
+COM_POS_OPP_C_ROBOT1 = 60;
+COM_POS_OPP_C_ROBOT2 = 61;
+COM_POS_OPP_C_ROBOT3 = 62;
+COM_POS_OPP_C_ROBOT4 = 63;
+COM_REFEREE;
 
-COM_ACTION_ROBOT1 = 1; //walking
-COM_ACTION_ROBOT2 = 0; //stationary
-COM_ACTION_ROBOT3 = 1; //walking
+usleep(1);
+//RECEIVED VARIABLES FROM DECISION
+A = DECISION_STATE;
+B = DECISION_POSITION_A;
+C = DECISION_POSITION_B;
+D = DECISION_POSITION_C;
+E = DECISION_BALL_POS;
+F = DECISION_OPP1_POS;
+G = DECISION_OPP2_POS;
+H = DECISION_OPP3_POS;
+}
 
+#ifdef DEBUG
+cout << A <<" "<< B <<" "<< C <<" "<< D <<" "<<E <<" "<< F <<" "<< G<<" "<< H << endl;
+#endif
 
+}
 }
