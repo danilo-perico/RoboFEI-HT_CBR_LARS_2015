ef7359764c0ceaf699925e1e0e118ebb6b9b9031
Danilo Perico <danilo.perico@gmail.com>
2015-10-16 17:41:07 -0300

    change all start_*.sh because they were not working properly -- seems to work now! At least in Debian :-)

--- start_all/start_all.sh
+++ start_all/start_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/RoboFEI-HT/build/bin
 
-echo "starting all processes"
+sudo echo "starting all processes"
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/RoboFEI-HT/build/lib
 # export PATH=$PATH:~/RoboFEI-HT/build/bin
 # source #HOME/.bashrc
@@ -10,31 +10,31 @@ while true
 do
     if [ ! "$(pidof imu)" ] 
     then
-       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_imu.sh' &
+       gnome-terminal -x sh -c './start_imu.sh' &
    fi
-   if [ ! "$(pidof -x start_vision.sh)" ] 
-   then
-          gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_vision.sh' &
-   fi
+#   if [ ! "$(pidof -x start_vision.sh)" ] 
+#   then
+#          gnome-terminal -x sh -c 'echo 123456 | sudo -S ./start_vision.sh'
+#   fi
     if [ ! "$(pidof -x start_decision.sh)" ]  
     then
-        gnome-terminal -x sh -c './start_decision.sh'  &
+        gnome-terminal -x sh -c './start_decision.sh'
     fi
-    if [ ! "$(pidof control)" ] 
-    then
-       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_control.sh' &
-    fi
-    if [ ! "$(pidof communication)" ] 
-    then
-       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_comm.sh' &
-    fi
-    if [ ! "$(pidof commServer)" ] 
-    then
-       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_comm.sh' &
-    fi
-    if [ ! "$(pidof CommClient)" ] 
-    then
-       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./home/fei/RoboFEI-HT/start_all/start_comm.sh' &
-    fi
+#    if [ ! "$(pidof control)" ] 
+#    then
+#       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./start_control.sh'
+#    fi
+#    if [ ! "$(pidof communication)" ] 
+#    then
+#       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./start_comm.sh'
+#    fi
+#    if [ ! "$(pidof commServer)" ] 
+#    then
+#       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./start_comm.sh'
+#    fi
+#    if [ ! "$(pidof CommClient)" ] 
+#    then
+#       gnome-terminal -x sh -c 'echo 123456 | sudo -S ./start_comm.sh'
+#    fi
     sleep 10
 done
--- start_all/start_comm.sh
+++ start_all/start_comm.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 #!/RoboFEI-HT/build/bin
 
 echo "communication"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/RoboFEI-HT/build/lib
-export PATH=$PATH:~/RoboFEI-HT/build/bin
-source #HOME/.bashrc
+
 communication
