595125b0cd8275f3342e68ed66f264a0078bd4eb
isaac25silva <isaac25silva@yahoo.com.br>
2015-10-22 14:42:36 -0200

    add while in search

--- Decision/src/behavior.py
+++ Decision/src/behavior.py
@@ -170,7 +170,7 @@ class Ordinary(TreatingRawData):
                 self.set_stand_still()
                 if self.get_lost_ball_status() == 1: #1 - lost ball
                    self.set_turn_right()
-                   time.sleep(3)
+                   time.sleep(2)
                    self.set_stand_still()
             else:
                 if self.get_lost_ball_status() == 1:
